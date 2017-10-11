04960cd97d3e108b7f12996a3d3caf4e2badb09c
2-HP\2 <hunter00243@naver.com>
2017-10-12 08:52:03 +0900

    no message

---
+++ mystudy/src/main/java/com/sjinc/MystudyApplication.java
@@ -0,0 +1,12 @@
+package com.sjinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystudyApplication.class, args);
+	}
+}
